cd6442d601d947fab6904e8ed75c8eec3c43d3b0
Abel-cordero <abelcorderotineo99@gmail.com>
2025-06-11 23:39:04 -0500

    Add modular project scaffolding

---
+++ src/beam_model.py
@@ -0,0 +1,10 @@
+"""Data model representing a reinforced concrete beam."""
+
+class BeamModel:
+    """Encapsulates beam geometry, loads and reinforcement."""
+
+    def __init__(self):
+        self.sections = []
+        self.bars = []
+        self.layers = []
+
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+"""Helper utilities for unit conversions and styling."""
+
+
+def color_for_diameter(diam):
+    """Return a color associated with a rebar diameter."""
+    return "#000000"
+
