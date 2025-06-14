6b08ae064e51fb619ede136fd9f27b66afd8ff6d
Abel-cordero <abelcorderotineo99@gmail.com>
2025-06-14 08:50:35 -0500

    Make correct_moments static

--- tests/test_moment_app.py
+++ tests/test_moment_app.py
@@ -10,8 +10,8 @@ def test_correct_moments_dual1_dual2():
     mn = np.array([-10.0, -20.0, -15.0])
     mp = np.array([5.0, 2.0, 3.0])
 
-    mn_c1, mp_c1 = MomentApp.correct_moments(None, mn, mp, 'dual1')
-    mn_c2, mp_c2 = MomentApp.correct_moments(None, mn, mp, 'dual2')
+    mn_c1, mp_c1 = MomentApp.correct_moments(mn, mp, 'dual1')
+    mn_c2, mp_c2 = MomentApp.correct_moments(mn, mp, 'dual2')
 
     assert np.allclose(mn_c1, [-10.0, -20.0, -15.0])
     assert np.allclose(mn_c2, [-10.0, -20.0, -15.0])
