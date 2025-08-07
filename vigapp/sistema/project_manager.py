45648bf6a30815ac65420a6d08c67a783e4cc99c
Abel-cordero <abelcorderotineo99@gmail.com>
2025-08-07 18:21:22 -0500

    Implement JSON project save/load

--- vigapp/sistema/project_manager.py
+++ vigapp/sistema/project_manager.py
@@ -5,8 +5,35 @@ class ProjectManager:
     """Handles serialization of beam configurations."""
 
     def save(self, model, path):
-        """Save the model to disk."""
+        """Save the model to disk.
+
+        Parameters
+        ----------
+        model : dict
+            Data structure describing the project.
+        path : str or PathLike
+            Destination file where the model will be stored.
+        """
+        import json
+
+        with open(path, "w", encoding="utf-8") as fh:
+            json.dump(model, fh)
 
     def load(self, path):
-        """Load the model from disk."""
+        """Load the model from disk.
+
+        Parameters
+        ----------
+        path : str or PathLike
+            Source file previously created with :meth:`save`.
+
+        Returns
+        -------
+        dict
+            Reconstructed project model.
+        """
+        import json
+
+        with open(path, "r", encoding="utf-8") as fh:
+            return json.load(fh)
 
